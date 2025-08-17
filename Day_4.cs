855a9dfb649684129c9cdcadafd14802e76f4e6f
Raksha-jk <rakshajk1904@gmail.com>
2025-08-17 19:00:31 +0530

    Day 3 and 4

---
+++ Day_4.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of students : ");
+            int num = int.Parse(Console.ReadLine());
+            string[] names = new string[num];
+            int[] marks = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Enter the name of student " + (i + 1) + " : ");
+                names[i] = Console.ReadLine();
+                Console.WriteLine("Enter the marks of student " + (i + 1) + " : ");
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Names in Uppercase:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name.ToUpper());
+            }
+            Console.WriteLine("Names in Lowercase:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name.ToLower());
+            }
+            Console.WriteLine("Names and name length:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name + " : " + name.Length);
+            }
+            Console.WriteLine("Enter the name to search: ");
+            string searchName = Console.ReadLine();
+            int index = Array.IndexOf(names, searchName);
+            Console.WriteLine(names[index] + " secures " + marks[index]);
+            Console.WriteLine("Highest mark secured by");
+            int inmaxMark = Array.IndexOf(marks, marks.Max());
+            Console.WriteLine(names[inmaxMark] + " secures " + marks[inmaxMark]);
+            Console.WriteLine("Names in ascending order:");
+            Array.Sort(names);
+            foreach (string name in names)
+            {
+                Console.Write(name);
+            }
+            Console.WriteLine("\nNames in descending order:");
+            Array.Reverse(names);
+            foreach (string name in names)
+            {
+                Console.Write(name);
+            }
+            Console.WriteLine("Vowel names");
+            foreach (string name in names)
+            {
+                if (name.StartsWith("A", StringComparison.OrdinalIgnoreCase) ||
+                    name.StartsWith("E", StringComparison.OrdinalIgnoreCase) ||
+                    name.StartsWith("I", StringComparison.OrdinalIgnoreCase) ||
+                    name.StartsWith("O", StringComparison.OrdinalIgnoreCase) ||
+                    name.StartsWith("U", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Day_5.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> bookTitles = new List<string>();
+            Dictionary<string, int> books = new Dictionary<string, int>();
+            HashSet<string> borrowedMembers = new HashSet<string>();
+            int choice;
+            while (true)
+            {
+                Console.WriteLine("1. Add Book\n2. Remove Book\n3. Borrow Book\n4. Return Book\n5. View All Books\n6. View Borrowed Members\n7. Exit");
+                Console.Write("Enter your choice : ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter book title: ");
+                        string title = Console.ReadLine();
+                        if (books.ContainsKey(title))
+                        {
+                            books[title]++;
+                        }
+                        else
+                        {
+                            books.Add(title, 1);
+                            bookTitles.Add(title);
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Enter book title to remove: ");
+                        string removeTitle = Console.ReadLine();
+                        if (books.ContainsKey(removeTitle))
+                        {
+                            books[removeTitle]--;
+                            if (books[removeTitle] <= 0)
+                            {
+                                books.Remove(removeTitle);
+                                bookTitles.Remove(removeTitle);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Book not found.");
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter book title to borrow: ");
+                        string borrowTitle = Console.ReadLine();
+                        Console.Write("Enter member name: ");
+                        string memberName = Console.ReadLine();
+                        if (books.ContainsKey(borrowTitle) && books[borrowTitle] > 0)
+                        {
+                            books[borrowTitle]--;
+                            borrowedMembers.Add(memberName);
+                            Console.WriteLine($"{memberName} borrowed {borrowTitle}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Book not available for borrowing.");
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Enter book title to return: ");
+                        string returnTitle = Console.ReadLine();
+                        Console.Write("Enter member name: ");
+                        string returnMember = Console.ReadLine();
+                        if (books.ContainsKey(returnTitle))
+                        {
+                            books[returnTitle]++;
+                            borrowedMembers.Remove(returnMember);
+                            Console.WriteLine($"{returnMember} returned {returnTitle}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Book not found.");
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("All Books:");
+                        foreach (var book in books)
+                        {
+                            Console.WriteLine($"{book.Key} - {book.Value} copies");
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine("Borrowed Members:");
+                        foreach (var member in borrowedMembers)
+                        {
+                            Console.WriteLine(member);
+                        }
+                        break;
+                    case 7:
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
